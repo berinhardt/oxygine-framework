5f2320d3c8d71428fc91f27751a2652f10213a28
berinhardt <darta@web2mil.com.uy>
2022-05-02 13:16:59 -0300

    f

--- oxygine/src/oxygine/res/ResAnim.cpp
+++ oxygine/src/oxygine/res/ResAnim.cpp
@@ -6,7 +6,7 @@
 
 namespace oxygine {
 static AnimationFrame emptyFrame;
-Vector2 ResAnim::NOSIZE(1, 1)
+Vector2 ResAnim::NOSIZE(1, 1);
 ResAnim::ResAnim(Resource* atlas) : _columns(1), _atlas(atlas), _scaleFactor(1.0f), _appliedScale(1.0f), _framerate(30) {}
 
 ResAnim::~ResAnim() {}
